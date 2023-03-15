cac658fcfeb1876101c85a6d4939d7d6fcd44401
duy thanh <you@example.com>
2023-03-15 13:48:53 +0700

    bai1

---
+++ Baif1.js
@@ -0,0 +1,4 @@
+let listDay=["chủ nhật", "thứ hai", "thứ ba", "thứ tư", "thứ năm", "thứ sáu", "thứ bảy"];
+let dateIndext=new Date().getDay()
+// console.log(dateIndext);
+console.log(`Hom nay la :`+listDay[dateIndext]);
